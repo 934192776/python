ea4179abba8495dfc37492ebef91f0fa2a22a4d7
jinghui <934192776@qq.com>
2026-06-17 10:56:20 +0800

    update data type.py

--- Python Grammer/data type.py
+++ Python Grammer/data type.py
@@ -1,3 +1,36 @@
 print(type("jing hui"))
 print(type(888))
 print(type(79.53))
+
+# type
+name = "jing hui"
+name_type = type(name)
+print(name_type)
+
+
+# transfer string to int
+num_str = str(11)
+print(type(num_str), num_str)
+
+# transfer string to number
+float_str = str(11.88)
+print(type(float_str), float_str)
+num = int("11")
+print(type(num), num)
+num = float("11.88")
+print(type(num), num)
+
+# code identifier
+name_ = "jing hui"
+_name = "jing hui"
+name_1 = "jing hui"
+Itheima = "jing hui"
+itheima = "888"
+print(name_)
+
+firstnumber = 8
+studentnickname = "jing hui" 
+
+first_number = "8"
+student_nickname = "jing hui"
+print(first_number, student_nickname)
